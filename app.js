1fa7de522b7d41b9ec0ded064dff997a5659ad2f
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-01-12 10:55:49 +0530

    added basic repo

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Welcome to the world of Node.js")
+
+function getDetails(message, name){
+    return message + ' ' + name
+}
+
+console.log(getDetails("Welcome", "Uma"))
+
+console.log('I have modified the file')
